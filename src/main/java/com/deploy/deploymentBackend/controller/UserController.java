1fe27f2b1687161a26b7c9f7f29a130b8c581647
DhanushkaSa <dhanushkasa45@gmail.com>
2025-08-02 17:31:33 +0530

    Add cross origin

--- src/main/java/com/deploy/deploymentBackend/controller/UserController.java
+++ src/main/java/com/deploy/deploymentBackend/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.deploy.deploymentBackend.entity.User;
 import com.deploy.deploymentBackend.service.UserService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
